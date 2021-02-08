d058e7499855272431fed676f848120b9f07aa53
Hadi-Darmawan <ghadi.darmawan@gmail.com>
2021-02-08 21:44:48 +0800

    Menambahkan comment pada AboutUsActivity.java

--- app/src/main/java/id/hadidev/WaterFlowMonitoring/Activities/AboutUsActivity.java
+++ app/src/main/java/id/hadidev/WaterFlowMonitoring/Activities/AboutUsActivity.java
@@ -24,6 +24,8 @@ public class AboutUsActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.appToolbar);
         setSupportActionBar(toolbar);
 
+        // Digunakan untuk menampilkan tanda panah back
+        // di bagian pojok kiri atas layar
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
@@ -37,11 +39,16 @@ public class AboutUsActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        // Digunakan untuk melakukan melakukan switch antar halaman dari halaman about
+        // ke halaman Setting Notifikasi
         switch (item.getItemId()){
             case R.id.settingNotification:
                 intent = new Intent(AboutUsActivity.this, NotificationSettingActivity.class);
                 startActivity(intent);
                 break;
+
+            // Digunakan untuk menampilkan notifikasi jika user menekan kembali pilihan menu About
+            // padahal user telah berada di menu about
             case R.id.aboutUs:
                 Toast.makeText(AboutUsActivity.this, "You're in About Us Page", Toast.LENGTH_SHORT)
                         .show();
